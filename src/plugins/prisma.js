b9ad987fe699090f57897ade76dd44046bb61361
rahul <rahuls@svu.ac.in>
2026-02-24 16:37:17 +0530

    fix: set prisma output path in schema + fix PrismaClient import path for Vercel

--- src/plugins/prisma.js
+++ src/plugins/prisma.js
@@ -1,7 +1,8 @@
 import fp from 'fastify-plugin';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../generated/prisma/index.js';
 import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
+
 
 /**
  * Prisma Plugin for Fastify
